593f063e333b83dc6a4023410b22f295cac35858
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-11-10 01:15:45 +0600

    Api db.js updated

--- api/db.js
+++ api/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql')
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'atcsystem'
+    password: 'root',
+    database: 'toll_management'
 })
 
 module.exports = db;
